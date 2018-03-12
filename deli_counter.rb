20907182054e59e2b2d9bcfa6dc02babad995f58
Emily Welsch <emwelsch@gmail.com>
2018-03-12 15:58:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,9 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
-    print "The line is currently:"
+    puts "The line is currently:"
     katz_deli.each_with_index {|name, index|
-      print " #{index+1}. #{name}"}
+      puts " #{index+1}. #{name}"}
   end
 end
 # line(katz_deli)
